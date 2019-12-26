1a7a77c1b88d6588201a5c3997f10862fd3a266c
Romain Capocasale <romain.capocasale@he-arc.ch>
2019-12-26 17:08:47 +0100

    add project

---
+++ TournamentApp/ArcOthelloAIstub/OthelloIA2/AlphaBetaAlgo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OthelloAIstub
+{
+    class AlphaBetaAlgo
+    {
+        public static Tuple<int, Tuple<int, int>> Alphabeta(TreeNode root, int depth, int minOrMax, int parentvalue)
+        {
+            if (depth == 0 || root.isFinal())
+            {
+                return new Tuple<int, Tuple<int, int>>(root.Score(), null);
+            }
+
+            int optVal = minOrMax * -int.MaxValue;
+            Tuple<int, int> optOp = null;
+
+            foreach (Tuple<int, int> move in root.ListOps())
+            {
+                TreeNode newNode = root.ApplyOp(move);
+
+                Tuple<int, Tuple<int, int>> newMove = Alphabeta(newNode, depth - 1, -minOrMax, optVal);
+
+                if ((newMove.Item1 * minOrMax) > (optVal * minOrMax))
+                {
+                    optVal = newMove.Item1;
+                    optOp = move;
+
+                    if ((optVal * minOrMax) > (parentvalue * minOrMax))
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return new Tuple<int, Tuple<int, int>>(optVal, optOp);
+        }
+    }
+}
---
+++ TournamentApp/ArcOthelloAIstub/OthelloIA2/TreeNode.cs
@@ -0,0 +1,68 @@
+﻿using IAStub;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OthelloAIstub
+{
+    class TreeNode
+    {
+        private bool isWhitePlayer;
+        private OthelloBoard board;
+
+        public TreeNode(OthelloBoard board, bool isWhitePlayer)
+        {
+            this.isWhitePlayer = isWhitePlayer;
+            this.board = board;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int[,] theBoard = board.GetBoard();
+            int playerVal = isWhitePlayer ? 1 : 0;
+            for (int i = 0; i < theBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < theBoard.GetLength(1); j++)
+                {
+                    int boardValue = theBoard[i, j];
+                    if (boardValue == playerVal)
+                    {
+                        score +=  OthelloBoard.SCORE_MATRIX[i, j];
+                    }
+                    else
+                    {
+                        score -= OthelloBoard.SCORE_MATRIX[i, j]; 
+                    }
+                }
+            }
+
+            if(score < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return score;
+            }
+        }
+        public TreeNode ApplyOp(Tuple<int, int> move)
+        {
+            board.PlayMove(move.Item1, move.Item2, isWhitePlayer);
+            OthelloBoard newOB = new OthelloBoard(board.GetBoard());
+            return new TreeNode(newOB, !isWhitePlayer);
+        }
+
+        public List<Tuple<int,int>> ListOps()
+        {
+            return board.GetPossibleMove(isWhitePlayer);
+        }
+
+        public bool isFinal()
+        {
+            return board.GetPossibleMove(isWhitePlayer).Count == 0;
+        }
+    }
+}
